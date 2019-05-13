fcaca5b3f0f619de41b8b7f9e48ebee5fb57cf83
Larsvandertier <45361740+Larsvandertier@users.noreply.github.com>
2019-05-13 15:02:30 +0200

    make title

--- resources/views/adressen/index.blade.php
+++ resources/views/adressen/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layout')
 @section('content')
+    <h1> Adressen</h1>
     @foreach($adressen as $adres)
         <ol>
             <li>{{$adres->voornaam}} {{$adres->tussenvoegsel}} {{$adres->achternaam }}</li>
